08661ebeff88276426f040350034330fcfbb8f94
goupmon <59721731+goupmon@users.noreply.github.com>
2026-05-17 02:39:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from APPUiO Cloud!"
+    return "Hello!"
+
+@app.route("/swisscom-sbtkpoc")
+def contact():
+    return "Sourav Sahana"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
